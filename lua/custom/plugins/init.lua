6a3bc021b1f38a7308f35845b899e89284f05755
Sebastian <seppo2606@gmail.com>
2023-07-28 17:35:20 +0200

    add autopairs + telescope keybind

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,12 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
+
+
+vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.format()]]
+
+vim.keymap.set('n', '<leader>ff', require('telescope.builtin').find_files, { desc = '[F]ind [f]iles' })
+vim.keymap.set('n', '<leader>fb', vim.lsp.buf.format, { desc = '[F]ind [b]uffer' })
+
+
 return {}
