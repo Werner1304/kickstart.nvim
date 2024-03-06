66d18a317acbcea9e8cc78adeb0bc2ae08fc687b
Werner1304 <seppo2606@gmail.com>
2024-03-06 18:05:47 +0100

    add nvim-tree

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -14,7 +14,7 @@ vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.format()]]
 
 vim.keymap.set('n', '<leader>ff', require('telescope.builtin').find_files, { desc = '[F]ind [f]iles' })
 vim.keymap.set('n', '<leader>fb', vim.lsp.buf.format, { desc = '[F]ind [b]uffer' })
-vim.keymap.set('n', '<leader>t', vim.cmd.Ex, { desc = '[T]ree' })
+-- vim.keymap.set('n', '<leader>t', vim.cmd.Ex, { desc = '[T]ree' })
 vim.keymap.set('n', '<leader>sv', vim.cmd.vsplit, { desc = '[S]plit [v]ertical' })
 vim.keymap.set('n', '<leader>sh', vim.cmd.split, { desc = '[S]plit [h]orizontal' })
 
